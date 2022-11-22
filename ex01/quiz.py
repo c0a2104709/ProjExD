31fb2ad8a3271ed07206e1896bc8be693d8f608d
konishi <c0a2104709@edu.teu.ac.jp>
2022-11-22 16:06:38 +0900

    クイズ完成

---
+++ ex01/quiz.py
@@ -0,0 +1,25 @@
+import random
+import datetime
+def shutudai(qa_lst):
+    qa = random.choice(qa_lst)
+    print("問題:"+qa["q"])
+    return qa["a"]
+def kaitou(ans_lst):
+    st = datetime.datetime.now()
+    ans = input("答えるんだ：")
+    ed = datetime.datetime.now()
+    if ans in ans_lst:
+        print("正解！！！")
+    else:
+        print("出直してこい")
+    print("回答時間：" + (ed-st).seconds)
+
+if __name__ == "__main__":
+    qa_lst = [
+        {"q":"サザエの旦那の名前は？","a":{"ますお","マスオ"}},
+        {"q":"カツオの妹の名前は？","a":{"ワカメ","わかめ"}},
+        {"q":"タラオはカツオから見てどんな関係？","a":{"甥","おい","甥っ子","おいっこ"}}
+    ]
+
+    ans_lst = shutudai(qa_lst)
+    kaitou(ans_lst)
