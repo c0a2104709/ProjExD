42683374925c21779a4fe41594373b3b213ecc64
konishi <c0a2104709@edu.teu.ac.jp>
2022-11-29 15:35:47 +0900

    数字の順番を変えた

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,8 @@
+#moduleのインポート
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+#ボタンをクリックしたときの動作
 def button_click(event):
     btn = event.widget
     num = btn["text"]
@@ -14,28 +16,34 @@ def button_click(event):
 root = tk.Tk()
 root.geometry("300x500")
 
+
 entry = tk.Entry(root, justify="right", width=10, font=("", 40))
 entry.grid(row=0, column=0, columnspan=3)
 
+#0~9までの数字を表示する
 r = 1
-c = 0
+c = 2
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row= r, column=c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
+    c -= 1
+    if c == -1:
         r+=1
-        c=0
+        c=2
+    if num == 1:
+        c = 1
 
+#+と=を表示する
+c = 2
 operators = ["+", "="]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
+    c -= 2
+    if c == -1:
         r+=1
-        c=0
+        c=2
 
 root.mainloop()
