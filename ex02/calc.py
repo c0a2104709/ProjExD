ef8a0adf9020c424fdd83d5be1e02c2651dfe338
konishi <c0a2104709@edu.teu.ac.jp>
2022-11-29 14:53:51 +0900

    練習7

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,7 +5,10 @@ def button_click(event):
     btn = event.widget
     num = btn["text"]
     if num == "=":
-        pass
+        siki = entry.get()
+        res = eval(siki)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
     else:
         entry.insert(tk.END, num)
 root = tk.Tk()
