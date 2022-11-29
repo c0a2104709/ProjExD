8bdc5bf917857548596711ff74882ce77554c242
konishi <c0a2104709@edu.teu.ac.jp>
2022-11-29 16:44:59 +0900

    クリアボタンと（と）を追加した

--- ex02/calc.py
+++ ex02/calc.py
@@ -11,10 +11,12 @@ def button_click(event):
         res = eval(siki)
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    elif num == "C":
+        entry.delete(0, tk.END)
     else:
         entry.insert(tk.END, num)
 root = tk.Tk()
-root.geometry("400x500")
+root.geometry("480x500")
 
 
 entry = tk.Entry(root, justify="right", width=10, font=("", 40))
@@ -35,22 +37,25 @@ for num in range(9, -1, -1):
         c = 1
 
 #+と=を表示する
-c = 3
-r = 2
-operators = ["**2", "**3", "**", "+", "="]
+c = 4
+r = 1
+operators = ["C", "(", ")", "", "", "**2", "**3", "**", "+", "="]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
-    c -= 1
-    if r != 4:
-        c = 3
-        r += 1
+    r += 1
 
-    if c == -1:
-        r+=1
-        c=2
-    if c == 1:
+    if r == 5:
+        if c < 4:
+            if c == 2:
+                c = 0
+                r = 4
+                continue
+            c -= 1
+            r = 4
+            continue
+        r = 1
         c -= 1
 
 root.mainloop()
