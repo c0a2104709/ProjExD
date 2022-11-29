ae42127e8e3ae6af60d397e8a6227848b46bd7fc
konishi <c0a2104709@edu.teu.ac.jp>
2022-11-29 14:35:58 +0900

    練習4

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,7 +9,10 @@ def button_click(event):
 root = tk.Tk()
 root.geometry("300x500")
 
-r = 0
+entry = tk.Entry(root, justify="right", width=10, font=("", 40))
+entry.grid(row=0, column=0, columnspan=3)
+
+r = 1
 c = 0
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
