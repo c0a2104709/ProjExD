677fda7c4488f8fd8e9ae0048cc8150951b52f59
konishi <c0a2104709@edu.teu.ac.jp>
2022-12-06 17:14:55 +0900

    修正版

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,19 +13,19 @@ def main_proc():
     global kokaton
     global cx, cy, mx, my
     if key ==  "Up":
-        kokaton = tk.PhotoImage(file="fig/6.png")
+        kokaton = tk.PhotoImage(file="fig/6.png")#上を向いているこうかとんの画像を描画
         my -= 1
     if key == "Down":
-        kokaton = tk.PhotoImage(file="fig/4.png")
+        kokaton = tk.PhotoImage(file="fig/4.png")#下を向いているこうかとんの画像を描画
         my += 1
     if key == "Left":
-        kokaton = tk.PhotoImage(file="fig/5.png")
+        kokaton = tk.PhotoImage(file="fig/5.png")#左を向いているこうかとんの画像を描画
         mx -= 1
     if key == "Right":
-        kokaton = tk.PhotoImage(file="fig/2.png")
+        kokaton = tk.PhotoImage(file="fig/2.png")#右を向いているこうかとんの画像を描画
         mx += 1
     if maze_lst[mx][my] == 1: #移動先が壁だったら
-        kokaton = tk.PhotoImage(file="fig/0.png")
+        kokaton = tk.PhotoImage(file="fig/0.png") #?を浮かべているこうかとんの画像を描画
         if key ==  "Up":
             my += 1
         if key == "Down":
@@ -39,7 +39,7 @@ def main_proc():
                         image=kokaton, 
                         tag="kokaton")
     canvas.coords("kokaton", cx, cy)
-    if cx == 13*100+50 and cy == 7*100+50:
+    if cx == 13*100+50 and cy == 7*100+50: #右下のマスについたら終了させる
         return
     root.after(100, main_proc)
 
@@ -58,7 +58,7 @@ if __name__ == "__main__":
                         image=kokaton, 
                         tag="kokaton")
 
-    goal = tk.PhotoImage(file="fig/9.png")
+    goal = tk.PhotoImage(file="fig/9.png") #右下のマスにゴール地点としてこうかとんの画像を描画
     canvas.create_image(13*100+50, 7*100+50,
                         image=goal,
                         tag="goal")
