94d9f38dae0717a0a269f9db6d47d23c2b9eea47
konishi <c0a2104709@edu.teu.ac.jp>
2022-12-06 14:27:34 +0900

    練習3

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,4 +5,9 @@ if __name__ == "__main__":
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width= 1500, height=900, bg="black")
     canvas.pack()
+    kokaton = tk.PhotoImage(file="fig/8.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, 
+                        image=kokaton, 
+                        tag="kokaton")
     root.mainloop()
