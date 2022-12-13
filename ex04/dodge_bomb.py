5a506abd68e4319c41f51708cdd1eb0f4f1f84b3
konishi <c0a2104709@edu.teu.ac.jp>
2022-12-13 15:26:15 +0900

    練習4-7&4-8

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,18 @@ import pygame as pg
 import sys
 import random
 
+#練習７
+def check_bound(obj_rct, scr_rct):
+    # 第１引数：こうかとんrectまたは爆弾rect
+    # 第２引数：スクリーンrect
+    # 範囲内：+1/範囲外：-1
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
+
 def main():
     clock = pg.time.Clock()
     #練習１
@@ -27,6 +39,7 @@ def main():
     bomb_rct.centerx = random.randint(0, scrn_rct.width)
     bomb_rct.centery = random.randint(0, scrn_rct.height)
     scrn_sfc.blit(bomb_sfc, bomb_rct)
+    vx, vy = +1, +1
 
     #練習２
     while True:
@@ -45,13 +58,30 @@ def main():
             tori_rct.centerx -= 1
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
+        
+        if check_bound(tori_rct, scrn_rct) != (+1, +1):
+            # どこかしらはみ出ていたら
+            if key_dct[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_dct[pg.K_DOWN]:
+                tori_rct.centery -= 1
+            if key_dct[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_dct[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
         scrn_sfc.blit(tori_sfc, tori_rct)
 
         #練習６
-        vx, vy = 1, 1
         bomb_rct.move_ip(vx, vy)
-
         scrn_sfc.blit(bomb_sfc, bomb_rct)
+
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+
+        #練習８
+        if tori_rct.colliderect(bomb_rct):
+            return
 
         pg.display.update()
         clock.tick(1000)
