2c21111a90c31278385fd4055f9fd8d44ad30a5e
konishi <c0a2104709@edu.teu.ac.jp>
2022-12-13 14:47:47 +0900

    練習4-5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,11 +1,13 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
     #練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig\pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
@@ -16,6 +18,15 @@ def main():
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
     scrn_sfc.blit(tori_sfc, tori_rct)
+
+    #練習５
+    bomb_sfc = pg.Surface((20, 20))
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct)
 
     #練習２
     while True:
@@ -35,6 +46,8 @@ def main():
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
         scrn_sfc.blit(tori_sfc, tori_rct)
+
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
 
         pg.display.update()
         clock.tick(1000)
