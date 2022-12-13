05793faeec4df1dc6b6cad7cc20812f12a7003c5
konishi <c0a2104709@edu.teu.ac.jp>
2022-12-13 14:17:09 +0900

    練習4-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,11 +2,22 @@ import pygame as pg
 import sys
 
 def main():
+    clock = pg.time.Clock()
     #練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
-    backscrn_sfc = pg.image.load("fig\pg_bg.jpg")
-    
+    pgbg_sfc = pg.image.load("fig\pg_bg.jpg")
+    pgbg_rct = pgbg_sfc.get_rect()
+
+    #練習２
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT: #ウインドウの✖ボタンがクリックされたら
+                return
+        pg.display.update()
+        clock.tick(1000)
+
 
 if __name__ == "__main__":
     pg.init()
